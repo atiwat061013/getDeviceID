3d3abefee789a4c1183b1caa9c787cc30c0df3c3
atiwat_t <atiiwwat@gmail.com>
2021-07-21 13:43:37 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+////////////////////////////////////////////////////////////////
+import { Device } from '@ionic-native/device/ngx';
+import { NetworkInterface } from '@ionic-native/network-interface/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Device,
+    NetworkInterface,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { Device } from '@ionic-native/device/ngx';
+import { Platform } from '@ionic/angular';
+import { NetworkInterface } from '@ionic-native/network-interface/ngx';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  deviceID: any;
+
+
+  constructor(
+    private device: Device,
+    private platform: Platform,
+    private networkInterface: NetworkInterface,
+  ) {
+
+    this.networkInterface.getWiFiIPAddress()
+    .then(address => console.info(`IP: ${address.ip}, Subnet: ${address.subnet}`))
+    .catch(error => console.error(`Unable to get IP: ${error}`));
+
+  this.networkInterface.getCarrierIPAddress()
+    .then(address => console.info(`IP: ${address.ip}, Subnet: ${address.subnet}`))
+    .catch(error => console.error(`Unable to get IP: ${error}`));
+
+  const url = 'www.github.com';
+  this.networkInterface.getHttpProxyInformation(url)
+    .then(proxy => console.info(`Type: ${proxy.type}, Host: ${proxy.host}, Port: ${proxy.port}`))
+    .catch(error => console.error(`Unable to get proxy info: ${error}`));
+
+
+    if (this.platform.is('android')) {
+      console.log('Android');
+      this.deviceID = this.device.uuid;
+      console.log('Device UUID is: ' + this.device.uuid);
+    } else if (this.platform.is('electron')) {
+      this.deviceID = 'GET-MAC';
+    } else if (this.platform.is('desktop')) {
+      this.deviceID = 'GET-Desktop';
+
+    }
+
+    
+
+  }
+
+
+
+  
+    
+
+
 
 }
